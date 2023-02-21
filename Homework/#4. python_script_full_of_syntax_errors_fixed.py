08abb73afd1fd7a344af06e5b3dab2b01df582d0
Fatima Gutierez <fag4902@mavs.uta.edu>
2023-02-20 21:11:27 -0600

    commit

---
+++ Homework/#4. python_script_full_of_syntax_errors_fixed.py
@@ -0,0 +1,20 @@
+a = 2                 The 1 in front of the a messes up the variable a since we are having a equal something else
+a1 = b                This works because there is nothing in front of the a instead its after the a
+x = 2  
+y = x + 4 # is it 6?  The X does not work, must be x in order to having the function running
+from math import tan  Math does not work only math 
+print (tan(pi))       Must have () around tan and pi since you are printing that statement and also ""
+pi = '3.14159’        Cannot use " and then ', you must use one only
+print (tan('pi'))     Must include the '' because pi is not a string
+c = 4**3**2**3
+_ = ((c-78564)/c + 32)  Not add an extra () in the end
+discount = 12         % is not used the same in jupyter notebook
+AMOUNT = 120.-1       Cannot leave it blank after the -
+amount = 120          $ is not used the same on jupyter notebook
+address = "hpl@simula.no"   Must add "" around what the address is
+a = "duck"            and is a command on jupyter notebook, therefore it cannot be used as a variable, change it to a
+class = 'INF1100, gr 2"   Cannot use ' and then use ", must be only one
+x=1, continue_ = x > 0     Must have a variable for x must, for example x=1, now the code will work
+rev = fox = True
+Persian = ['a human language']
+true = fox is rev in Persian  
